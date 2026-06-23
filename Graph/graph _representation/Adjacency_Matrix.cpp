40f7a838f114b0c2af55457b1d56020d2f81c4e4
amit <105482312+amit2725@users.noreply.github.com>
2026-06-23 17:32:57 +0530

    Implement adjacency matrix representation of a graph using c++
    This program implements an adjacency matrix representation of a graph. It takes user input for the number of nodes and edges, and then fills the matrix based on the connected edges.

--- Graph/graph _representation/Adjacency_Matrix.cpp
+++ Graph/graph _representation/Adjacency_Matrix.cpp
@@ -1,1 +1,28 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m; // n node, m edge:-
+    cout << "enter the total number of node";
+    cin >> n;
+    cout << "enter the number of edges";
+    cin >> m;
+   vector<vector<int>> ans(n + 1, vector<int>(n + 1, 0));
+    cout << "enter the connected edge";
+    for (int i = 0; i < m; i++)
+    {
+        int p, q;
+        cin >> p >> q;
+        ans[p][q] = 1;
+        ans[q][p] = 1;
+    }
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < n + 1; j++)
+        {   
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
