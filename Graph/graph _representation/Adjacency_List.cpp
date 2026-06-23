e289747c8e834a5b8eb2ce3101777722b1e38e40
amit <105482312+amit2725@users.noreply.github.com>
2026-06-23 17:34:42 +0530

    Implement adjacency list representation for graph

--- Graph/graph _representation/Adjacency_List.cpp
+++ Graph/graph _representation/Adjacency_List.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"enter the number of node:- ";
+    cin>>n;
+    cout<<"enter the number of edge:- ";
+    cin>>m;
+    vector<vector<int>>ans(n+1);
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        ans[u].push_back(v);
+        ans[v].push_back(u);
+    }
+    for(int i=1;i<=n;i++){
+        cout<<i<<" -> ";
+        for(int x: ans[i]){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
+
 
